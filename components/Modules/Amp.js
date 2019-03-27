3766ec6619236526df3230db5999cd831b00c318
Spencer Bateman <bateman.s@husky.neu.edu>
2019-03-27 19:02:12 -0400

    add initial Synth Parent and Amp component

---
+++ components/Modules/Amp.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Fader from './../UI/Fader';
+
+class Amp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gain: 0
+    }
+
+    this.updateGain = this.updateGain.bind(this)
+  }
+
+  updateGain = (gain) => {
+    this.setState({
+      gain: gain
+  }, () => {
+    this.props.update(this.state)
+  })
+  }
+
+  render() {
+    return(
+      <div>
+        <Frame>
+          <SynthName>Fader 1.0</SynthName>
+          <Fader
+            label="Gain"
+            min={-20}
+            max={0}
+            default={0}
+            units="dB"
+            updateFrequency={this.updateGain}
+          />
+        </Frame>
+      </div>
+    );
+  }
+}
+
+const SynthName = styled.div`
+  font-size: 20px;
+  grid-row: 1;
+  grid-column: 1;
+  font-family: 'Work Sans',sans-serif;
+  text-transform: Uppercase;
+  margin: 0 0 0 20px;
+`
+
+const Frame = styled.div`
+  border-radius: 10px;
+  border: 3px solid black;
+  display: Grid;
+  grid-template-columns: 200px 200px 200px;
+  grid-template-rows: 50px 250px;
+  align-items: center;
+  background: #fcfcfc;
+`
+
+export default Amp;
+
--- components/Synthesizer/Synthesizer.js
+++ components/Synthesizer/Synthesizer.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Fader from './../UI/Fader';
-import WaveSelector from './../UI/WaveSelector';
 import WAVEFORMS from  './../Synthesizer/WAVEFORMS';
 import Oscillator from  './../Modules/Oscillator';
+import Amp from  './../Modules/Amp';
 
 class Synthesizer extends React.Component {
   constructor(props) {
@@ -12,11 +11,12 @@ class Synthesizer extends React.Component {
     this.state = {
       waveform: WAVEFORMS.SINE.id,
       frequency : 440,
-      duration : 100
+      duration : 100,
+      gain: 10
     }
-
     this.playSound = this.playSound.bind(this)
     this.update = this.update.bind(this)
+    this.setGain = this.setGain.bind(this)
   }
 
   componentDidMount() {
@@ -42,11 +42,20 @@ class Synthesizer extends React.Component {
     })
   }
 
+  setGain(state) {
+    this.setState({
+      gain: state.gain
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
   render() {
     return(
     <div>
       <Layout>
         <Oscillator update={this.update} />
+        <Amp update={this.setGain}/>
       </Layout>
     <button onClick={this.playSound}>Play!</button>
   </div>
